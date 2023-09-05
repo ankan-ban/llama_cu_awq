a71e4c29129a22bcb6d569a5288cff2f1006c754
ankan-ban <ankan.ban@gmail.com>
2023-09-06 00:05:10 +0530

    fix hardcoded filenames in convert_awq_to_bin.py

--- convert_awq_to_bin.py
+++ convert_awq_to_bin.py
@@ -4,11 +4,20 @@ import torch
 # Import os module
 import os
 
-# Load the file
-data = torch.load("llama2-7b-awq.pt")
+# Import sys module
+import sys
 
-# Create a sub-directory named 'bin_wt' if it does not exist
-os.makedirs('bin_wt', exist_ok=True)
+# Get the filename from the command line argument
+filename = sys.argv[1]
+
+# Get the directory name from the command line argument
+dirname = sys.argv[2]
+
+# Load the data from the filename
+data = torch.load(filename)
+
+# Create a sub-directory with the given name if it does not exist
+os.makedirs(dirname, exist_ok=True)
 
 # If the data is a dictionary, iterate over its keys and values
 if isinstance(data, dict):
@@ -17,6 +26,6 @@ if isinstance(data, dict):
     # If the value is a tensor, check its shape and dtype
     if isinstance(value, torch.Tensor):
       print(value.shape, value.dtype)
-      # Dump the tensor to a binary file with the same name as the key
-      with open(os.path.join('bin_wt', key + '.bin'), 'wb') as f:
+      # Dump the tensor to a binary file with the same name as the key in the given directory
+      with open(os.path.join(dirname, key + '.bin'), 'wb') as f:
         f.write(value.numpy().tobytes())
